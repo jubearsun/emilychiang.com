e81435fc0aa3407df30880d149446e2e894587ab
Julia Sun <julia.sun@berkeley.edu>
2015-12-21 17:08:00 -0800

    Initial commit

---
+++ src/js/app.js
@@ -0,0 +1,32 @@
+var app = angular.module('appName', [
+        'ui.router',
+        'ngTouch'
+    ]);
+    
+app.run([
+    '$rootScope',
+    '$state',
+    '$stateParams',
+    function ($rootScope, $state, $stateParams) {
+        $rootScope.$state = $state;
+        $rootScope.$stateParams = $stateParams;
+    }
+]);
+
+app.config(['$stateProvider',
+    '$urlRouterProvider',
+    '$locationProvider',
+    function($stateProvider, $urlRouterProvider, $locationProvider) {
+        $locationProvider.html5Mode(true);
+        $urlRouterProvider.otherwise("/");
+
+        $stateProvider
+            .state('home', {
+                url: '/',
+                templateUrl: './views/home.html',
+                data: {
+                    pageTitle: 'Title'
+                }
+            });
+    }
+]);
